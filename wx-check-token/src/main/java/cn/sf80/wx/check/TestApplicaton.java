d1f57e80164228887ae11f40a88b33f7ea851d32
wcjwctwy <708808508@qq.com>
2017-11-17 00:53:24 +0800

    init commit

---
+++ wx-check-token/src/main/java/cn/sf80/wx/check/TestApplicaton.java
@@ -0,0 +1,13 @@
+package cn.sf80.wx.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplicaton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplicaton.class,args);
+    }
+
+}
